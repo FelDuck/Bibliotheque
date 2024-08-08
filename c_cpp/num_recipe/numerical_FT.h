46184fc5d09072bb0849da19f1e8fe782031e7fe
Felix Theriault <felixthdev@gmail.com>
2024-08-07 20:03:38 -0400

    Delete c_cpp/num_recipe directory
    shoulnt have commited

--- c_cpp/num_recipe/numerical_FT.h
+++
@@ -1,6 +0,0 @@
-/*
-Je voulais vraiment vraiment implementer mes propres version de ces methodes mais comme il est rendu 22h22 le 30 avril j'ai comme pas le choix
-*/
-
-extern void splint(float* xa, float* ya, float* y2a,int n,float x, float* y);
-extern void spline(float* x, float* y,int n,float yp1,float ypn,float* y2);
